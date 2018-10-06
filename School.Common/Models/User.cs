d48a6cfb98da2bd38e8c487723a445dd92383302
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2018-10-06 10:29:50 -0500

    add class user

---
+++ School.Common/Models/User.cs
@@ -0,0 +1,74 @@
+﻿namespace School.Common.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+
+        [Display(Name = "E-Mail")]
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(50)]//only you can to write 50 characters
+        [DataType(DataType.EmailAddress)]
+        // [Index("UserNameIndex", Isnique = true)]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", this.FirstName, this.LastName); }
+        }
+
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+       
+        [Display(Name = "Is Student")]
+        public bool IsStudent { get; set; }
+
+        [Display(Name = "Is Teacher")]
+        public bool IsTeacher { get; set; }
+
+        [Display(Name = "Image")]
+        public string Photo { get; set; }
+
+        [Display(Name = "Image")]
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(Photo))
+                {
+                    return "noimage";
+                }
+
+                return string.Format(
+                    "http://landsapi1.azurewebsites.net/{0}",
+                    Photo.Substring(1));
+            }
+        }
+        
+
+
+       
+    }
+}
+
